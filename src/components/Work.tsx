f17ec806ad70ff0c7fcc61786b36f2c68f29d9ee
Alex-Prokhorenko <galexthunder@gmail.com>
2023-02-11 18:27:10 +0700

    feat: add react-icons library. change settings button to gear icon

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsGearFill } from "react-icons/bs";
 import Button from "./Button";
 import styles from "./styles/Work.module.css"
 
@@ -27,10 +28,10 @@ const Work = ({
     return <div className={isWork ? "bg-red-900" : "bg-lime-900"}>
         <div className={styles.work_component}>
             <div className={styles.settings_block}>
-                <Button name="Settings" onClick={handleSettings}/>
+                <BsGearFill onClick={handleSettings} className="text-6xl text-lime-600"/>
                 <div >
                     {isCounting
-                        && <img src={require("./img/2GU.gif")} alt="work" width="65px"/>}
+                        && <img src={require("./img/2GU.gif")} alt="work" width="58px"/>}
                 </div>
             </div>
             <div className={styles.clocks}>{minutes}:{seconds}</div>
